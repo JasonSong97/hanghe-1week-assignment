6027c3c6e84b1729e31730b92018acc738e1bc2f
JasonSong97 <jaegeunsong97@gmail.com>
2024-12-18 00:55:10 +0900

    TEST: [성공] 포인트 조회 성공

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -1,4 +1,17 @@
 package io.hhplus.tdd.point;
 
+import org.springframework.stereotype.Service;
+
+import io.hhplus.tdd.database.UserPointTable;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class PointService {
+
+    private final UserPointTable userPointTable;
+
+    public UserPoint getPoint(long userId) {
+        return userPointTable.selectById(userId);
+    }
 }
---
+++ src/test/java/io/hhplus/tdd/point/PointServiceTest.java
@@ -0,0 +1,45 @@
+package io.hhplus.tdd.point;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.hhplus.tdd.database.UserPointTable;
+
+public class PointServiceTest {
+    
+    @Mock
+    private UserPointTable userPointTable;
+
+    @InjectMocks
+    private PointService pointService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this); // 테스트별 어노테이션 초기화
+    }
+
+    @Test
+    void 포인트_조회_성공() throws Exception {
+        // given
+        final long userId = 1L;
+        final UserPoint expectedPoint = new UserPoint(userId, 5000L, System.currentTimeMillis());
+
+        when(userPointTable.selectById(userId)).thenReturn(expectedPoint);
+
+        // when
+        UserPoint result = pointService.getPoint(userId);
+
+        // then
+        assertThat(result).isEqualTo(expectedPoint);
+        verify(userPointTable, times(1)).selectById(userId);
+    }
+}
