86749ef09a43e78db2f2c0356a4ecf04b4f6a2f4
JasonSong97 <jaegeunsong97@gmail.com>
2024-12-19 01:27:36 +0900

    Impl [성공] 포인트 충전에 성공한다.

--- src/main/java/io/hhplus/tdd/point/PointServiceImpl.java
+++ src/main/java/io/hhplus/tdd/point/PointServiceImpl.java
@@ -4,16 +4,28 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import io.hhplus.tdd.database.PointHistoryTable;
+import io.hhplus.tdd.database.UserPointTable;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class PointServiceImpl implements PointService {
+
+    private final PointHistoryTable pointHistoryTable;
+    private final UserPointTable userPointTable;
     
     @Override
-    public UserPoint chargeUserPoint(long id, long amount) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'chargeUserPoint'");
+    public UserPoint chargeUserPoint(long userId, long amount) {
+        // 사용자 조회
+        UserPoint PSuserPoint = userPointTable.selectById(userId);
+        // 조회된 사용자 포인트 증가
+        UserPoint updatedUserPoint = PSuserPoint.increaseUserPoints(amount);
+        // 충전내역 저장
+        pointHistoryTable.insert(userId, amount, TransactionType.CHARGE, System.currentTimeMillis());
+        // 변경된 사용자 업데이트
+        userPointTable.insertOrUpdate(updatedUserPoint.id(), updatedUserPoint.point());
+        return updatedUserPoint;
     }
 
     @Override
---
+++ src/test/java/io/hhplus/tdd/point/PointServiceImplTest.java
@@ -0,0 +1,55 @@
+package io.hhplus.tdd.point;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import io.hhplus.tdd.database.PointHistoryTable;
+import io.hhplus.tdd.database.UserPointTable;
+
+@ExtendWith(MockitoExtension.class) // Mockito 확장을 통해 Mockito가 테스트에서 사용할 목업 객체를 주입할 수 있도록 설정
+public class PointServiceImplTest {
+    
+    @Mock private PointHistoryTable pointHistoryTable;
+    @Mock private UserPointTable userPointTable;
+
+    @InjectMocks private PointServiceImpl pointServiceImpl; // 목 객체를 주입 받을 구현체, 실제 테스트 대상
+
+    @Test
+    @DisplayName(value = "Impl [성공] 포인트 충전에 성공한다.")
+    void 포인트_충전_성공케이스() throws Exception {
+        // given
+        long userId = 1L;
+        long currentAmount = 1_000L;
+        long chargeAmount = 4_000L;
+
+        UserPoint userPoint = new UserPoint(userId, currentAmount, System.currentTimeMillis());
+        UserPoint updatedPoint = new UserPoint(userId, currentAmount + chargeAmount, System.currentTimeMillis());
+
+        // when
+        when(userPointTable.selectById(eq(userId)))
+            .thenReturn(userPoint);
+        when(userPointTable.insertOrUpdate(eq(userId), eq(currentAmount + chargeAmount)))
+            .thenReturn(updatedPoint);
+        UserPoint result = pointServiceImpl.chargeUserPoint(userId, chargeAmount);
+
+        // then
+        assertNotNull(result);
+        assertEquals(currentAmount + chargeAmount, result.point());
+
+        verify(userPointTable, times(1)).selectById(eq(userId));
+        verify(userPointTable).insertOrUpdate(eq(userId), eq(currentAmount + chargeAmount));
+        verify(pointHistoryTable).insert(eq(userId), eq(chargeAmount), eq(TransactionType.CHARGE), anyLong());
+    }
+}
