6baf556525e766c39fd743817238c678bf897d6e
JasonSong97 <jaegeunsong97@gmail.com>
2024-12-18 23:56:09 +0900

    SETTING: PointServiceImpl, PointService 기초 세팅

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -2,12 +2,6 @@ package io.hhplus.tdd.point;
 
 import java.util.List;
 
-import org.springframework.stereotype.Service;
-
-import io.hhplus.tdd.database.PointHistoryTable;
-import io.hhplus.tdd.database.UserPointTable;
-import lombok.RequiredArgsConstructor;
-
 public interface PointService {
 
     UserPoint chargeUserPoint(long id, long amount);
@@ -17,23 +11,4 @@ public interface PointService {
     UserPoint findUserPoint(long id);
 
     List<PointHistory> findUserHistory(long id);
-
-    // private final UserPointTable userPointTable;
-    // private final PointHistoryTable pointHistoryTable;
-
-    // public UserPoint getPoint(long userId) {
-    //     UserPoint userPoint = userPointTable.selectById(userId);
-    //     if (userPoint == null) {
-    //         throw new IllegalArgumentException("존재하지 않는 사람압니다.");
-    //     }
-    //     return userPointTable.selectById(userId);
-    // }
-
-    // public List<PointHistory> getUserPointHistory(long userId) {
-    //     List<PointHistory> pointHistoryList = pointHistoryTable.selectAllByUserId(userId);
-    //     if (pointHistoryList == null) {
-    //         throw new IllegalArgumentException("존재하지 않는 사람입니다.");
-    //     }
-    //     return pointHistoryTable.selectAllByUserId(userId);
-    // }
 }
--- src/main/java/io/hhplus/tdd/point/PointServiceImpl.java
+++ src/main/java/io/hhplus/tdd/point/PointServiceImpl.java
@@ -1,5 +1,37 @@
 package io.hhplus.tdd.point;
 
-public class PointServiceImpl {
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class PointServiceImpl implements PointService {
+    
+    @Override
+    public UserPoint chargeUserPoint(long id, long amount) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'chargeUserPoint'");
+    }
+
+    @Override
+    public UserPoint useUserPoint(long userId, long amount) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'useUserPoint'");
+    }
+
+    @Override
+    public UserPoint findUserPoint(long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findUserPoint'");
+    }
+
+    @Override
+    public List<PointHistory> findUserHistory(long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findUserHistory'");
+    }
     
 }
